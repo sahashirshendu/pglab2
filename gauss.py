361467754f6dc29bb4e46f81847d6f28475a42b2
Shirshendu Saha <shirshendusaha.ss@gmail.com>
2023-11-01 17:29:48 +0530

    Commit @ 2023-11-01 17:29:48

--- gauss.py
+++ gauss.py
@@ -10,10 +10,10 @@ def f(x):
     return 1 / (2*pi*s**2)**.5 * exp(-((x-u)/s)**2/2)
 
 # Composite Trapezoidal
-h = (b - a) / n
-sum = f(a) + f(b)
-for i in range(1, n):
-    sum = sum + 2 * f(a + i * h)
-sum = h / 2 * sum
+h = (b-a)/n
+sum = f(a)+f(b)
+for i in range(1,n):
+    sum = sum+2*f(a+i*h)
+sum = h/2*sum
 
 print('Probability =', sum)
