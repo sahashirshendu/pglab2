cffa0e896808b0ecc1eaa6913f68408ca151ae75
Shirshendu Saha <shirshendusaha.ss@gmail.com>
2023-11-03 21:33:47 +0530

    Commit @ 2023-11-03 21:33:47

--- gauss.py
+++ gauss.py
@@ -2,6 +2,7 @@ from math import exp, pi
 
 u = float(input('Mean = '))
 s = float(input('Standard Deviation = '))
+# change a and b as needed
 a = u-s
 b = u+s
 n = 100
@@ -9,7 +10,7 @@ n = 100
 def f(x):
     return 1 / (2*pi*s**2)**.5 * exp(-((x-u)/s)**2/2)
 
-# Composite Trapezoidal
+# Composite Trapezoid
 h = (b-a)/n
 sum = f(a)+f(b)
 for i in range(1,n):
