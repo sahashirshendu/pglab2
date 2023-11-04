5401aad54afce3a36658077f18b44e4cef525256
Shirshendu Saha <shirshendusaha.ss@gmail.com>
2023-11-04 19:47:32 +0530

    Commit @ 2023-11-04 19:47:32

--- SET1/5b.py
+++ SET1/5b.py
@@ -1,19 +1,23 @@
 from math import exp, pi
+from numpy import linspace
 
 u=120 # mean
 s=20 # standard deviation
-a=130
-b=1000
+a=-1000
 n=5000
 
 def f(x):
     return 1/(2*pi*s**2)**.5 * exp(-((x-u)/s)**2/2)
 
-# Composite Trapezoid
-h=(b-a)/n
-sum=f(a)+f(b)
-for i in range(1,n):
-    sum=sum+2*f(a+i*h)
-sum=h/2*sum
-
-print('Probability=',sum)
+x=linspace(0,500,5000)
+for b in x:
+    # Composite Trapezoid
+    h=(b-a)/n
+    sum=f(a)+f(b)
+    for i in range(1,n):
+        sum=sum+2*f(a+i*h)
+    sum=h/2*sum
+    print(b,abs(sum-0.835))
+    if abs(sum-0.835)<=1e-3:
+        print('Value of d is',b)
+        break
