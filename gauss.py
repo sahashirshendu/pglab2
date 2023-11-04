450cb90ffce3f921ddd8135ea0f93b877456cd2a
Shirshendu Saha <shirshendusaha.ss@gmail.com>
2023-11-04 18:47:45 +0530

    Commit @ 2023-11-04 18:47:45

--- gauss.py
+++ gauss.py
@@ -1,20 +1,20 @@
 from math import exp, pi
 
-u = float(input('Mean = '))
-s = float(input('Standard Deviation = '))
+u=float(input('Mean = '))
+s=float(input('Standard Deviation = '))
 # change a and b as needed
-a = u-s
-b = u+s
-n = 100
+a=u-s
+b=u+s
+n=100
 
 def f(x):
-    return 1 / (2*pi*s**2)**.5 * exp(-((x-u)/s)**2/2)
+    return 1/(2*pi*s**2)**.5 * exp(-((x-u)/s)**2/2)
 
 # Composite Trapezoid
-h = (b-a)/n
-sum = f(a)+f(b)
+h=(b-a)/n
+sum=f(a)+f(b)
 for i in range(1,n):
-    sum = sum+2*f(a+i*h)
-sum = h/2*sum
+    sum=sum+2*f(a+i*h)
+sum=h/2*sum
 
-print('Probability =', sum)
+print('Probability=',sum)
