3f32f8666e81ade6ac103b6ece9ccb132ca20d05
Shirshendu Saha <shirshendusaha.ss@gmail.com>
2023-11-05 12:13:02 +0530

    S1 Done

--- SET1/1a.py
+++ SET1/1a.py
@@ -7,13 +7,14 @@ def fact(n):
 def ncr(n,r):
     return fact(n)/(fact(n-r)*fact(r))        
 
-tot=int(input('Enter total number of balls- ')) # 35
-rd=float(input('Enter red in red:orange ratio- ')) # 3
-on=float(input('Enter orange in red:orange ratio- ')) # 2
-pk=float(input('Enter probability of a pink ball- ')) # 0.4285714285714286
-ro=rd/on
-num_p=int(tot*pk)
-num_o=(tot-num_p)*on/(rd+on)
+tot=int(input('Enter total number of balls- '))
+ro1=float(input('Enter red in red:orange ratio- '))
+ro2=float(input('Enter orange in red:orange ratio- '))
+pk1=float(input('Enter a for a/b for probability of a pink ball- '))
+pk2=float(input('Enter b for a/b for probability of a pink ball- '))
+ro=ro1/ro2
+num_p=tot*pk1/pk2
+num_o=(tot-num_p)*ro2/(ro1+ro2)
 num_r=(tot-num_p)-num_o
-p=ncr(int(num_o),1)*ncr(int(num_p),1)/ncr(35,2)
+p=ncr(int(num_o),1)*ncr(int(num_p),1)/ncr(tot,2)
 print('Probability that one ball is orange and one ball is pink=',p)
--- SET1/2a.py
+++ SET1/2a.py
@@ -1,10 +1,13 @@
-def root(a,b,c):
-    if b**2-4*a*c>=0:
-        x1=(-b-(b**2-4*a*c)**.5)/(2*a)
-        x2=(-b+(b**2-4*a*c)**.5)/(2*a)
-        return [x1,x2]
-    else:
-        print('Roots are not real')
+# a*x**2+b*x+c=0
+a=2
+b=-75
+c=472
 
-x=root(2,-75,472)
+if b**2-4*a*c>=0:
+    x1=(-b-(b**2-4*a*c)**.5)/(2*a)
+    x2=(-b+(b**2-4*a*c)**.5)/(2*a)
+    x=[x1,x2]
+else:
+    print('Roots are not real')
+
 print('Total number of toffees in the first tiffin box:',int(2*x[0]-4))
