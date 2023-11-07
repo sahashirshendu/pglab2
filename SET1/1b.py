130df222d904ae119056aaeca39a41eed13c34b5
Shirshendu Saha <87955430+sahashirshendu@users.noreply.github.com>
2023-11-07 09:48:39 +0530

    Set 1 done (Edit)

--- SET1/1b.py
+++ SET1/1b.py
@@ -1,7 +1,7 @@
 # Students of A,B,C
-a=5
-b=6
-c=7
+a=int(input('Studrnts in Sec-A: '))
+b=int(input('Studrnts in Sec-B: '))
+c=int(input('Studrnts in Sec-C: '))
 t=a+b+c
 P=1/b*b/t # probability that it was the youngest of B
 print('Probability that it was the youngest of B:',P)
