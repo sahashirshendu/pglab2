44c7f2efb986290053c163401d1a51ea36e43cc6
Shirshendu Saha <shirshendusaha.ss@gmail.com>
2023-11-09 12:45:01 +0530

    Commit @ 2023-11-09 12:45:01

---
+++ SET1/4.py
@@ -0,0 +1,68 @@
+from numpy import *
+
+xy=loadtxt('4.txt')
+x=xy[:,0]
+y=xy[:,1]
+
+x1s=sum(x)
+x2s=0
+x3s=0
+x4s=0
+ys=sum(y)
+yxs=0
+yx2s=0
+for i in range(len(x)):
+    x2s=x2s+x[i]**2
+    x3s=x3s+x[i]**3
+    x4s=x4s+x[i]**4
+    yxs=yxs+x[i]*y[i]
+    yx2s=yx2s+x[i]**2*y[i]
+
+a=[[len(x),x1s,x2s],[x1s,x2s,x3s],[x2s,x3s,x4s]]
+b=[[ys],[yxs],[yx2s]]
+N=len(a)
+
+def cofactor(a,temp,p,q,n):
+    i=0
+    j=0
+    for k in range(n):
+        for m in range(n):
+            if k!=p and m!=q:
+                temp[i][j] = a[k][m]
+                j += 1
+                if j==n-1:
+                    j=0
+                    i=i+1
+
+def determinant(a,n):
+    D=0
+    if n==1:
+        return a[0][0]
+    temp=zeros((N,N))
+    sign=1
+    for f in range(n):
+        cofactor(a,temp,0,f,n)
+        D=D+sign*a[0][f]*determinant(temp,n-1)
+        sign=-sign
+    return D
+
+def inverse(a,inv):
+    sign=1
+    temp=zeros((N,N))
+    for i in range(N):
+        for j in range(N):
+            cofactor(a,temp,i,j,N)
+            sign=[1,-1][(i+j)%2]
+            inv[j][i]=sign*(determinant(temp, N-1))/determinant(a,N)
+
+ai=zeros((N,N))
+inverse(a, ai)
+s=zeros((N,1))
+for i in range(N):
+   for j in range(1):
+       for k in range(N):
+           s[i][j]=s[i][j]+ai[i][k]*b[k][j]
+
+print('Coefficients of x**0=',s[0])
+print('Coefficients of x**1=',s[1])
+print('Coefficients of x**2=',s[2])
