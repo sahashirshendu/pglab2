d6c360076e5b4e7ad9f46e738c24a7d5dce86044
Shirshendu Saha <shirshendusaha.ss@gmail.com>
2024-01-09 19:34:59 +0530

    Commit @ 2024-01-09 19:34:59

--- muon.py
+++ muon.py
@@ -1,8 +1,8 @@
 from pylab import *
 from scipy.optimize import curve_fit
 
-def f(t,a,b):
-    return b*exp(-t/a)
+def f(t,a,b,c):
+    return b*exp(-t/a)+c
 
 nbins=500
 file=loadtxt("file.txt")
@@ -14,7 +14,7 @@ for i in range(len(t)):
 
 tv,cv,_=hist(tl,1000)
 x=curve_fit(f,cv[0:len(cv)-1],tv)[0]
-plot(cv,f(cv,x[0],x[1]))
+plot(cv,f(cv,x[0],x[1],x[2]))
 xlabel('Time (ns)')
 ylabel('Number')
 show()
